6cdf36b74a59aa87154e7557463aeee413f46faf
dymafr <erwan1309@hotmail.com>
2020-12-05 17:52:37 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-c3-l5
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-c3-l5)
---
+++ src/app/cocktail-details/cocktail-details.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-cocktail-details",
+  templateUrl: "./cocktail-details.component.html",
+  styleUrls: ["./cocktail-details.component.scss"]
+})
+export class CocktailDetailsComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
